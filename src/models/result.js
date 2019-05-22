f89399d6bda9c158ab188c91c91b0385b7c41d19
dieson <dieson335@163.com>
2019-05-22 11:27:52 +0800

    添加result model

---
+++ src/models/result.js
@@ -0,0 +1,61 @@
+import { getExecutionResult } from '@/services/api';
+
+export default {
+    namespace: 'result',
+    state: {
+        data: {
+            list: [],
+            pagination: {},
+        },
+        code: undefined,
+    },
+
+    effect: {
+        *fetch({ payload }, { call, put }) {
+            const response = yield call(getExecutionResult, payload);
+
+            if (response.code === 2004) {
+                yield put({
+                    type: 'clear_list',
+                    payload: response,
+                });
+            } else {
+                const result = {
+                    code: response.code,
+                    data: {
+                        list: response.data.list,
+                        pagination: {
+                            total: response.data.total,
+                            pageSize: response.data.pageSize,
+                            currentPage: response.data.currentPage,
+                        },
+                    },
+                };
+
+                yield put({
+                    type: 'save',
+                    payload: result,
+                });
+            }
+
+        },
+    },
+
+    reducers: {
+        save(state, action) {
+            return {
+                ...state,
+                data: action.payload.data,
+                code: action.payload.code,
+            };
+        },
+
+        clear_list(state, action) {
+            return {
+                ...state,
+                code: action.payload.code,
+                data: { list: [] },
+            };
+        },
+    },
+}
